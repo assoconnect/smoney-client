0f9f0428f7be1b8798cc63594831dfd4217d5330
Sylvain Fabre <syl.fabre@gmail.com>
2020-11-24 18:41:34 +0100

    Code style

--- tests/Manager/BankCardManagerTest.php
+++ tests/Manager/BankCardManagerTest.php
@@ -19,7 +19,7 @@ class BankCardManagerTest extends SMoneyTestCase
 
         return new BankCardManager($client);
     }
- 
+
     public function testCreateRetrieveBankCardRegistration()
     {
         $client = $this->getClient();
